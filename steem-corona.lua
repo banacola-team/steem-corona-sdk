d0d1630a7ea34885dade043c17555316822c47cf
bobdos <>
2018-05-29 21:38:06 +1000

    add get_accounts api

--- steem-corona.lua
+++ steem-corona.lua
@@ -1,9 +1,9 @@
 module(..., package.seeall)
 
 function get_config(callback)
+
   local params = {}
-  params.body = ""
-  
+
   network.request(
     "https://api.steemjs.com/get_config",
     "GET",
@@ -12,9 +12,9 @@ function get_config(callback)
 end
 
 function get_version(callback)
+
   local params = {}
-  params.body = ""
-  
+
   network.request(
     "https://api.steemjs.com/get_version",
     "GET",
@@ -24,8 +24,7 @@ end
 
 function get_account_count(callback)
   local params = {}
-  params.body = ""
-  
+
   network.request(
     "https://api.steemjs.com/get_account_count",
     "GET",
@@ -33,4 +32,27 @@ function get_account_count(callback)
     params)
 end
 
+function get_accounts(account_array, callback)
 
+  local string_begin = "["
+  local string_end = "]"
+  local names = string_begin
+
+  for i = 1, #account_array do
+    if(i < #account_array) then
+      names = names .. "%22" .. account_array[i] .. "%22" .. ","
+    else
+      names = names .. "%22" .. account_array[i] .. "%22"
+    end
+  end
+
+  names = names .. string_end
+
+  local params = {}
+
+  network.request(
+    "https://api.steemjs.com/get_accounts?names="..names,
+    "GET",
+    callback,
+    params)
+end
--- test.lua
+++ test.lua
@@ -1,6 +1,9 @@
 local steem = require("steem-corona-sdk.steem-corona")
 local json = require("json")
 
+local account_array = {"steemsdk.test"}
+
+--[[ test case: get_config ]]
 steem.get_config(
   function(event)
     if event.isError then
@@ -8,8 +11,10 @@ steem.get_config(
     else
       print("response: "..event.response)
     end
-  end)
+  end
+)
 
+--[[ test case: get_version ]]
 steem.get_version(
   function(event)
     if event.isError then
@@ -17,12 +22,32 @@ steem.get_version(
     else
       print("response: "..event.response)
     end
-  end)
+  end
+)
 
-steem.get_account_count(function(event)
+--[[ test case: get_account_count ]]
+steem.get_account_count(
+  function(event)
     if event.isError then
       print( "Network error: ", event.response )
     else
       print("response: "..event.response)
     end
-  end)
+  end
+)
+
+--[[ test case: get_accounts ]]
+steem.get_accounts(
+  account_array,
+  function(event)
+    if event.isError then
+      print( "Network error: ", event.response )
+    else
+      print("response: "..json.prettify(event.response))
+      local decoded_response = json.decode(event.response)
+      print("id: "..decoded_response[1]["id"].."\n"..
+            "name: "..decoded_response[1]["name"])
+    end
+  end
+)
+
